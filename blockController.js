9b8dae69c4b500978fee705e5289f74a2f6a1da7
lalghoul <pro.alghoul@gmail.com>
2018-12-13 21:02:18 +0200

    Fixed validate wallet function and adding star proccess with handling errors

--- blockController.js
+++ blockController.js
@@ -5,6 +5,7 @@ const BlockClass = require("./Block.js");
 const BlockchainClass = require("./Blockchain.js");
 const bitcoin = require("bitcoinjs-lib");
 const bitcoinMessage = require("bitcoinjs-message");
+const hex2ascii = require("hex2ascii");
 const util = require("util");
 const TimeoutRequestsWindowTime = 5 * 60 * 1000;
 const ValidRequestWindowTime = 1800;
@@ -25,7 +26,7 @@ class BlockchainController {
     this.validateWallet();
     this.getBlockByIndex();
     this.postNewBlock();
-    this.registerStar();
+    //this.registerStar();
     this.requestValidation();
     this.initializeMockData();
   }
@@ -81,22 +82,6 @@ class BlockchainController {
   removeValidationRequestMempool(address) {
     delete this.mempoolValid[address];
   }
-  simpleStringify(object) {
-    var simpleObject = {};
-    for (var prop in object) {
-      if (!object.hasOwnProperty(prop)) {
-        continue;
-      }
-      if (typeof object[prop] == "object") {
-        continue;
-      }
-      if (typeof object[prop] == "function") {
-        continue;
-      }
-      simpleObject[prop] = object[prop];
-    }
-    return simpleObject; // returns cleaned up JSON
-  }
   verifyAddressRequest(address) {
     if (typeof this.mempoolValid[address] != "undefined") {
       return true;
@@ -107,10 +92,9 @@ class BlockchainController {
 
   requestValidation() {
     let self = this;
-    this.app.post("/requestValidation/:address", (req, res) => {
-      let address = req.params.address;
+    this.app.post("/requestValidation/", (req, res) => {
+      let address = req.body.address;
       if (typeof this.timeoutRequests[address] === "undefined") {
-        let address = req.params.address;
         let requestedTimestamp = new Date()
           .getTime()
           .toString()
@@ -162,80 +146,87 @@ class BlockchainController {
 
   validateWallet() {
     let self = this;
-    self.app.post(
-      "/message-signature/validate/:address/:signature",
-      (req, res) => {
-        res.header("Content-Type", "application/x-www-form-urlencoded");
-        let signature = decodeURIComponent(req.params.signature);
-        let address = decodeURIComponent(req.params.address);
-        if (!this.verifyAddressRequest(address)) {
-          if (typeof self.timeoutRequests[address] != "undefined") {
-            let isValid = bitcoinMessage.verify(
-              self.requests[address].message,
-              address,
-              signature
-            );
+    self.app.post("/message-signature/validate/", (req, res) => {
+      res.header("Content-Type", "application/x-www-form-urlencoded");
+      let signature = req.body.signature;
+      let address = req.body.address;
+      console.log(this.verifyAddressRequest(address));
+      if (!this.verifyAddressRequest(address)) {
+        if (typeof self.timeoutRequests[address] != "undefined") {
+          let isValid = bitcoinMessage.verify(
+            self.requests[address].message,
+            address,
+            signature
+          );
 
-            if (isValid) {
-              res.status(200);
+          if (isValid) {
+            res.status(200);
 
-              let status = {
-                address: address,
-                requestTimeStamp: this.requests[address].requestedTimestamp,
-                message: this.requests[address].message,
-                validationWindow: 1800,
-                messageSignature: true
-              };
-              this.mempoolValid[address] = status;
-              res.set("Status", JSON.stringify(this.mempoolValid[address]));
-              res.end();
-            } else {
-              res.set("Error", isValid);
-              res.set("Status", JSON.stringify(this.mempoolValid[address]));
-              res.end();
-            }
+            let status = {
+              address: address,
+              requestTimeStamp: this.requests[address].requestedTimestamp,
+              message: this.requests[address].message,
+              validationWindow: 1800,
+              messageSignature: true
+            };
+            this.mempoolValid[address] = status;
+            res.set("Status", JSON.stringify(this.mempoolValid[address]));
+            res.end();
           } else {
-            res.set("Sig", signature);
-            res.set("Address", address);
+            res.set("Error", isValid);
+            res.set("Status", JSON.stringify(this.mempoolValid[address]));
+            res.end();
+          }
+        } else {
+          res.set("Sig", signature);
+          res.set("Address", address);
+          res.set(
+            "Error",
+            "No request matched for this address! Please make a request"
+          );
+          res.end();
+        }
+      } else {
+        console.log("You already made a request. Please send your star data");
+        if (req.body.star != undefined) {
+          let star = JSON.parse(req.body.star);
+          address = req.body.address;
+          let starStory = star.story;
+          let storyCount = starStory.length;
+          if (this.verifyAddressRequest(address) && storyCount < 250) {
+            let body = {
+              address: address,
+              stardata: {
+                ra: star.ra,
+                dec: star.dec,
+                mag: star.mag,
+                cen: star.cen,
+                story: Buffer.from(starStory).toString("hex")
+              }
+            };
+
+            res.set("Status", "Star Successfully Added");
+            this.chain.addBlock(new BlockClass.Block(body)).then(block => {
+              let blockString = block;
+              blockString.storyDecoded = starStory;
+              let m = JSON.stringify(blockString);
+              this.removeValidationRequestMempool(address);
+              res.status(200);
+              res.set("Block", m);
+              res.end();
+            });
+          } else {
+            res.status(500);
             res.set(
-              "Error",
-              "No request matched for this address! Please make a request"
+              "Erorr",
+              "Sorry but we didnt find your address in the MEMPOOL"
             );
             res.end();
           }
         } else {
-          res.set("Error", "You already made a request.");
+          res.set("Error", "Please define your star");
           res.end();
         }
-      }
-    );
-  }
-  registerStar() {
-    this.app.post("/addstar", (req, res) => {
-      let star = JSON.parse(req.body.star);
-      let address = req.body.address;
-      let starStory = star.story;
-      if (!this.verifyAddressRequest(address)) {
-        let body = {
-          address: address,
-          stardata: {
-            ra: star.ra,
-            dec: star.dec,
-            mag: star.mag,
-            cen: star.cen,
-            story: starStory
-          }
-        };
-        res.status(200);
-        res.set("Status", "Star Successfully Added");
-        res.set("Block", JSON.stringify(body));
-        res.end();
-        console.log(body);
-        this.chain.addBlock(new BlockClass.Block(body));
-      } else {
-        res.status(500);
-        res.set("Erorr", "Somthing went wrong!!");
-        res.end();
       }
     });
   }
